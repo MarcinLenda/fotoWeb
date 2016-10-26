35902a66f07b0b1912c1d7110782f9bf466af431
MarcinLenda <777marcinlenda777@gmail.com>
2016-10-26 22:28:13 +0200

    test2

--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -1,104 +1,6 @@
 /**
  * Created by Promar on 17.10.2016.
  */
-
-app.service('service', function ($rootScope, $http, $location ) {
-
-    var self = this;
-    var _username = '';
-    $rootScope.userRoles = false;
-    $rootScope.logout = false;
-
-   var setUsername = function (username) {
-        _username = username;
-    };
-
-    this.showUserName = function () {
-        return "Jesteś zalogowany jako: "+_username;
-    };
-
-    console.log("tu"+$rootScope.userRoles);
-    $http({
-        method: 'GET',
-        url: '/myAccount/user'
-    }).then(function successCallback(response) {
-        var data = response.data;
-        $rootScope.authenticated = true;
-        setUsername(data.name);
-        if(data.name=='admin'){
-            $rootScope.userRoles = true;
-        }
-
-    }, function errorCallback(response) {
-        $rootScope.authenticated = false;
-        console.log('authenticated false');
-    });
-
-        this.authenticated = function(credentials, callback) {
-
-        var headers = credentials ? {
-            authorization : "Basic "
-            + btoa(credentials.username + ":"
-                + credentials.password)
-        } : {};
-
-
-        $http.get('/myAccount/user', {
-            headers : headers
-        }).then(function(response) {
-            var data = response.data;
-            if (data.name) {
-                $rootScope.authenticated = true;
-                setUsername(data.name);
-                $rootScope.admin = data && data.roles && data.roles.indexOf("ROLE_ADMIN")>-1;
-
-                if(data.name=='admin'){
-                    $rootScope.userRoles = true;
-                }
-
-            } else {
-                self.authenticated = false;
-                self.admin = false;
-            }
-            callback && callback(true);
-        }, function() {
-            self.authenticated = false;
-            callback && callback(false);
-        });
-    };
-
-    self.credentials = {};
-
-
-});
-
-app.service('fileUpload', ['$http', function ($http) {
-    var photoInfo = "";
-    var photoError = "";
-
-    this.uploadFileToUrl = function(file, nameAlbum, description){
-        var fd = new FormData();
-
-        fd.append('file', file);
-        fd.append('name', nameAlbum );
-        fd.append('descritpion',description );
-
-        $http.post("http://localhost:8080/photo/upload", fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined},
-            data: fd
-        })
-            .success(function(){
-                photoInfo = "Zdjęcie dodano do albumu:"+nameAlbum;
-            })
-            .error(function(){
-                photoError = "Nie udało się dodać zdjęcia";
-            });
-    };
-}]);/**
- * Created by Promar on 17.10.2016.
- */
-
 app.service('service', function ($rootScope, $http, $location ) {
 
     var self = this;
@@ -116,7 +18,7 @@ app.service('service', function ($rootScope, $http, $location ) {
 
     $http({
         method: 'GET',
-        url: '/myAccount/user'
+        url: 'http://52.39.52.69:8080/myAccount/user'
     }).then(function successCallback(response) {
         var data = response.data;
         $rootScope.authenticated = true;
